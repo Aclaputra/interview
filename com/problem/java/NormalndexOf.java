9bdddc51e03688aa31f42b0af7082cd359c2e17a
Aclaputra <muhamadacla22@gmail.com>
2022-08-05 01:57:13 +0700

    feat: added NormalIndexOf problem in java

---
+++ com/problem/java/NormalndexOf.java
@@ -0,0 +1,16 @@
+package com.problem.java;
+
+public class NormalndexOf {
+    public static void main(String args[])
+    {
+ 
+        // Initialising String
+        String gfg = new String("Welcome to geeksforgeeks");
+ 
+        System.out.print("Found g first at position : ");
+ 
+        // Initial index of 'g' will print
+        // prints 11
+        System.out.println(gfg.indexOf('g'));
+    }
+}

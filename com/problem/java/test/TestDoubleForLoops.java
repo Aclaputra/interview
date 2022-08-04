ca6e14472c44be1bc7a1fab259341cff6491295a
Aclaputra <muhamadacla22@gmail.com>
2022-08-05 01:57:37 +0700

    feat: added TestDoubleForLoops in java

---
+++ com/problem/java/test/TestDoubleForLoops.java
@@ -0,0 +1,16 @@
+package com.problem.java.test;
+
+public class TestDoubleForLoops {
+    static void forLoops(int n) {
+        for(int i = 0; i < n; i++) {
+            System.out.println("a = " + i);
+            for(int j = 0; j < n; j++) {
+                System.out.println("b = " + j);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        forLoops(11);
+    }
+}

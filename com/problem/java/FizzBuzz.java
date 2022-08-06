ad64c1a6621110b0c76ffe1be2390a7bf7b5b979
Aclaputra <muhamadacla22@gmail.com>
2022-08-06 12:13:28 +0700

    fixed fizzbuzz

--- com/problem/java/FizzBuzz.java
+++ com/problem/java/FizzBuzz.java
@@ -7,7 +7,6 @@ package com.problem.java;
 public class FizzBuzz {
     public static void main(String[] args) {
         int n = 100;
- 
         // loop for 100 times
         for (int i=1; i<=n; i++)                                
         {
@@ -20,12 +19,10 @@ public class FizzBuzz {
             // in place of the number
             else if (i%5==0)    
                 System.out.println("Buzz");
- 
             // number divisible by 3, print 'Fizz'
             // in place of the number
             else if (i%3==0)    
                 System.out.println("Fizz");
-                 
             else // print the numbers
                 System.out.println(i);                        
         }

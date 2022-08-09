3a373d53473366aabe1c50418125dde295e223ec
Aclaputra <muhamadacla22@gmail.com>
2022-08-09 10:55:08 +0700

    feat: added LinkedList

--- com/problem/java/dsa/LinkedList.java
+++ com/problem/java/dsa/LinkedList.java
@@ -1,5 +1,11 @@
 package com.problem.java.dsa;
 
 public class LinkedList {
-    
+    static void MyList(int n) {
+        System.out.println(n % 2 + 5 * 10);
+    }
+
+    public static void main(String[] args) {
+        MyList(5);
+    }
 }

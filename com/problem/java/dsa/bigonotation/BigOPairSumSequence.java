02296ec7a8658a5dd48b7e390daa3665891a3b75
Aclaputra <muhamadacla22@gmail.com>
2022-08-11 21:59:10 +0700

    feat: added bigopairsumsequence

--- com/problem/java/dsa/bigonotation/BigOPairSumSequence.java
+++ com/problem/java/dsa/bigonotation/BigOPairSumSequence.java
@@ -1,5 +1,20 @@
 package com.problem.java.dsa.bigonotation;
 
 public class BigOPairSumSequence {
-    
+    static int pairSumSequence(int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += pairSum(i, i + 1);
+        }
+        return sum;
+    }
+
+    static int pairSum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pairSumSequence(10));
+        System.out.println(pairSumSequence(5));
+    }
 }

5d2ae74ae160b8db05fc498993f8f9bcedd6ccaa
Aclaputra <muhamadacla22@gmail.com>
2022-08-24 05:59:32 +0700

    migrate doublebrace

---
+++ com/problem/java/hackerrank/DoubleBrace.java
@@ -0,0 +1,34 @@
+package com.problem.java.hackerrank;
+
+// Importing required classes
+import java.util.HashSet;
+import java.util.Set;
+
+public class DoubleBrace {
+    // Method 1
+    // Main driver method
+    public static void main(String[] args)
+    {
+        // Creating an empty HashSet of string entries
+        Set<String> sets = new HashSet<String>();
+ 
+        // Adding elements to Set
+        // using add() method
+        sets.add("one");
+        sets.add("two");
+        sets.add("three");
+ 
+        // Calling method 2 inside main() method
+        // Now pass above collection as parameter to method
+        useInSomeMethod(sets);
+    }
+ 
+    // Method 2
+    // Helper method
+    private static void useInSomeMethod(Set<String> sets)
+    {
+        // Print all elements of desired Set
+        // where method is invoked
+        System.out.println(sets);
+    }
+}

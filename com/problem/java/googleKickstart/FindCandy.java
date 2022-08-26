d7e1bb72c28c41dde16a3803a95de99b9d6ee447
Aclaputra <muhamadacla22@gmail.com>
2022-08-27 04:05:09 +0700

    feat: added googleKickstarts problem

---
+++ com/problem/java/googleKickstart/FindCandy.java
@@ -0,0 +1,13 @@
+package com.problem.java.googleKickstart;
+
+class FindCandy {
+  static int processCase(int case_num) {
+    return 9 + case_num;
+  }
+
+  public static void main(String[] args) {
+    // Read in the input for a single case
+    System.out.println(processCase(6));
+  }
+
+}

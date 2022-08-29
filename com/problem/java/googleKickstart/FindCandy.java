7f2cad507831a5afc380fd6d07d6cc90ccc766cf
Aclaputra <muhamadacla22@gmail.com>
2022-08-29 07:24:05 +0700

    feat: added FindCandy googleKickstart problem

--- com/problem/java/googleKickstart/FindCandy.java
+++ com/problem/java/googleKickstart/FindCandy.java
@@ -1,13 +1,36 @@
-package com.problem.java.googleKickstart;
+import java.util.Scanner;
 
-class FindCandy {
-  static int processCase(int case_num) {
-    return 9 + case_num;
-  }
+public class Solution {
+  static Scanner input = new Scanner(System.in);
 
   public static void main(String[] args) {
-    // Read in the input for a single case
-    System.out.println(processCase(6));
+    int T;
+    T = input.nextInt();
+
+    for(int test_case = 1; test_case <= T; test_case++) {
+      System.out.print("Case #" + test_case + ": ");
+      solve();
+    }
+    input.close();
   }
 
+  static void solve() {
+    int N, M;
+    N = input.nextInt();
+    M = input.nextInt();
+    int[] C = new int[N];
+
+    for (int i = 0; i < N; i++) {
+      C[i] = input.nextInt();
+    }
+
+    int totalCandies = 0;
+    for(int i = 0; i < N; i++) {
+      totalCandies += C[i];
+    }
+
+    int leftOutCandies = totalCandies % M;
+
+    System.out.println(leftOutCandies);
+  }
 }

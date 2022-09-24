8e5c6cc57531deb89721c5f06d4f6d1e29f14791
aclaputra <muhamadacla22@gmail.com>
2022-09-24 20:32:20 +0700

    feat: modified recordbreaker

--- com/problem/java/googleKickstart/2022/session/3/RecordBreaker.java
+++ com/problem/java/googleKickstart/2022/session/3/RecordBreaker.java
@@ -1,10 +1,10 @@
+import java.io.*;
 import java.util.*;
 
 public class Solution {
 
   public static int numberOfRecordBreakingDays(int numberOfDays, int[] visitors) {
     int recordBreakingDays = 0;
-
     // TODO: implement logic to calculate the number of record-breaking days
 
     return recordBreakingDays;

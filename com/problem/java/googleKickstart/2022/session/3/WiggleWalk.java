4deab090c2b41ebfdfde3d8ce8340a3f804a84f3
aclaputra <muhamadacla22@gmail.com>
2022-09-27 06:29:05 +0700

    feat: added wiggelwalk java

---
+++ com/problem/java/googleKickstart/2022/session/3/WiggleWalk.java
@@ -0,0 +1,15 @@
+import java.util.Scanner
+
+class Solution
+{
+  static Scanner in = Scanner(System.in);
+  public static void main(String[] args)
+  {
+    int t, n;
+    t = in.nextInt();
+    for (int i = 1; i < t; ++i)
+    {
+
+    }
+  }
+}

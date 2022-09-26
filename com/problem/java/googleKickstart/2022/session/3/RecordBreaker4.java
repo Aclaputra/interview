2165c3ddf642e7f3e527fb6f41692b2c3083edc4
aclaputra <muhamadacla22@gmail.com>
2022-09-27 00:43:11 +0700

    feat: addding recordbreaker4

---
+++ com/problem/java/googleKickstart/2022/session/3/RecordBreaker4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Solution {
+  static Scanner in = new Scanner(System.in);
+  static void main(String[] args) {
+    int t, n;
+    t = sc.nextInt();
+
+    for (int i = 1; i <= t; ++i)
+    {
+      n = sc.nextInt();
+      int arr[n], ans = 0, p = 0;
+      
+    }
+  }
+}

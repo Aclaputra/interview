a82fd12fc85e6201fd9d38bd0b21bb4a5d4e24d9
aclaputra <muhamadacla22@gmail.com>
2022-10-09 07:28:43 +0700

    feat: added codechef folder

---
+++ com/problem/java/codechef/BinarySubstituition.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class CodeChef
+{
+  public static void main(String[] args)
+  {
+    Scanner scanner = new Scanner(System.in);
+    int T = scanner.nextInt();
+
+    for (int i = 1; i <= T; i++)
+    {
+      String n = scanner.nextLine();
+      char[] letters = n.toCharArray();
+      for 
+      for (char letter : letters)
+      {
+        System.out.println(letter);
+      }
+
+      System.out.println("hasil");
+    }
+  }
+}

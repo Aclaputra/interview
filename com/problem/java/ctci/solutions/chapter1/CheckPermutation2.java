3ae7ba5129361567ba21ab1c7f3474d75d0acac1
aclaputra <muhamadacla22@gmail.com>
2022-10-19 01:07:25 +0700

    feat: added checkpermutation 2 java

---
+++ com/problem/java/ctci/solutions/chapter1/CheckPermutation2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class CheckPermutation 
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    String s = scanner.nextLine();
+    String t = scanner.nextLine();
+
+    boolean condition = false;
+    if (
+  }
+}

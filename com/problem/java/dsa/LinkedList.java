8965c96f841968a0582748b7f05e6ea224f22600
aclaputra <muhamadacla22@gmail.com>
2022-10-19 22:06:23 +0700

    feat: modified java dsa linkedlist

--- com/problem/java/dsa/LinkedList.java
+++ com/problem/java/dsa/LinkedList.java
@@ -1,11 +1,6 @@
 package com.problem.java.dsa;
 
 public class LinkedList {
-    static void MyList(int n) {
-        System.out.println(n % 2 + 5 * 10);
-    }
-
     public static void main(String[] args) {
-        MyList(5);
     }
 }

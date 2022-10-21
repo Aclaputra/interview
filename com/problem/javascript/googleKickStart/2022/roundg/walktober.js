75e76a9197b7ee1e6a372b29134fdf574bf8d8ff
aclaputra <muhamadacla22@gmail.com>
2022-10-21 18:26:08 +0700

    feat: modified walktober js

--- com/problem/javascript/googleKickStart/2022/roundg/walktober.js
+++ com/problem/javascript/googleKickStart/2022/roundg/walktober.js
@@ -1,1 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+  inputString = inputString.trim().split('\n').map(string => {
+    return string.trim();
+  });
+
+  main();
+});
+
+function readline() {
+  return inputString[currentLine++];
+}
+
+// Make a Snippet for the code above this and then write your logic in main();
+
+function main() {
+  // Declare and read the number of test cases.
+  var T;
+  T = parseInt(readline());
+  console.log(T);
+
+  // Loop over the number of test cases.
+  for (var test_no = 1; test_no <= T; test_no++) {
+    process.stdout.write('Case #' + test_no + ': ');
+    solve();
+  }
+}
+
+function solve() {
+  console.log("hi");
+}
 

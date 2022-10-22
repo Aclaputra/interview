89afdd7577b18f5e2196ed0ce445a0d2125ffd48
aclaputra <muhamadacla22@gmail.com>
2022-10-23 02:57:55 +0700

    feat: added curling 2 java

---
+++ com/problem/java/googleKickstart/2022/roundg/curling2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+  static Scanner sc = new Scanner(System.in);
+  static final PrintStream out = System.out;
+
+  static void solve() {
+
+    out.println("hi");
+  }
+  public static void main(String $[]) {
+    int T = sc.nextInt();
+    for (int t=1; t<=T; ++t) {
+      out.println("Case #"+t+": ");
+      solve();
+    }
+  }
+}

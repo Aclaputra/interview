d707d8f433fbafd15d33a65a6780321191a7ac30
Aclaputra <muhamadacla22@gmail.com>
2022-11-03 16:28:53 -0500

    feat: isUnique2 java has been added.

---
+++ com/problem/java/ctci/solutions/chapter1/IsUnique2.java
@@ -0,0 +1,10 @@
+package com.problem.java.ctci.solutions.chapter1;
+
+import java.util.Scanner;
+
+public class Solution {
+  static Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+
+  }
+}

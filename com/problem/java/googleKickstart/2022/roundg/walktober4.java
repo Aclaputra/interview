b4fc36b59c5ac861d19fc2eec2dd07f5031afe04
Aclaputra <muhamadacla22@gmail.com>
2022-12-11 15:17:33 +0700

    feat: added walktober4

---
+++ com/problem/java/googleKickstart/2022/roundg/walktober4.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+class Solution {
+  static Scanner sc = new Scanner(System.in);
+  static void solve() {
+    int N = sc.nextInt();
+    int M = sc.nextInt();
+    int P = sc.nextInt();
+    int ans = 0;
+
+    System.out.println("N: "+N+" M: "+M+" P: "+P);
+    // put all scores to scoreboard hashmap function
+
+  }
+
+  public static void main(String[] agrs) {
+    int T = sc.nextInt();
+    for (int t = 1; t <= T; ++t) {
+      //System.out.print("Case #"+ t +": ");
+      solve();
+    }
+    sc.close();
+  }
+}
+

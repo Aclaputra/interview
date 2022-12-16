d4076bd589370d17c2caff3357ab3a528b3ba74c
Aclaputra <muhamadacla22@gmail.com>
2022-12-16 12:31:06 +0700

    feat: moified recordbreaker2 java

--- com/problem/java/googleKickstart/2022/session/3/RecordBreaker2.java
+++ com/problem/java/googleKickstart/2022/session/3/RecordBreaker2.java
@@ -10,9 +10,11 @@ public class Solution {
     for (int i = 1; i <= t; i++)
     {
       int N, p = 0, count = 0;
+      // N equals number of days
       N = sc.nextInt();
+      // visitors per day
       int[] n = new int[N];
-
+      // get all input visitor perday
       for (int j = 0; j < N; j++)
       {
         n[j] = sc.nextInt();
@@ -20,12 +22,16 @@ public class Solution {
 
       for (int l = 0; l < N; l++)
       {
+        // greater than previous day
         boolean gpd = l == 0 || n[l] > p;
+        // greater than after day
         boolean gfd = l == N - 1 || n[l] > n[l + 1];
+        // if both true then increment the count (answer)
         if (gpd && gfd)
         {
           count++;
         }
+        // find maximum number between p and visitor by day
         p = Math.max(p, n[l]);
       }
       System.out.printf("Case #%d: %d\n", i, count);

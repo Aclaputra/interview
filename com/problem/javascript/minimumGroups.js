a97e978f15a4f4431853f35c1a8eb69a1121b6cf
Aclaputra <muhamadacla22@gmail.com>
2023-01-27 06:01:25 +0700

    feat: modified minimumgroups"

--- com/problem/javascript/minimumGroups.js
+++ com/problem/javascript/minimumGroups.js
@@ -2,34 +2,34 @@
 // group flips in a binary array
  
 function printGroups(arr, n) {
-    // Traverse through all array elements
-    // starting from the second element
+  // Traverse through all array elements
+  // starting from the second element
 
-    for (var i = 1; i < n; i++) {
-      // If current element is not same
-      // as previous
+  for (var i = 1; i < n; i++) {
+    // If current element is not same
+    // as previous
 
-      if (arr[i] != arr[i - 1]) {
-        // If it is same as first element
-        // then it is starting of the interval
-        // to be flipped.
+    if (arr[i] != arr[i - 1]) {
+      // If it is same as first element
+      // then it is starting of the interval
+      // to be flipped.
 
-        if (arr[i] != arr[0])
-          console.log( "From " + i + " to ");
-        // If it is not same as previous
-        // and same as first element, then
-        // previous element is end of interval
+      if (arr[i] != arr[0])
+        console.log( "From " + i + " to ");
+      // If it is not same as previous
+      // and same as first element, then
+      // previous element is end of interval
 
-        else
-          console.log(i - 1 + "<br>");
-      }
+      else
+        console.log(i - 1 + "<br>");
     }
-    // Explicitly handling the end of
-    // last interval
-    if (arr[n - 1] != arr[0])
-      console.log(n - 1 + "<br>");
   }
-   
-  var arr = [0, 1, 1, 0, 0, 0, 1, 1];
-  var n = arr.length;
-  printGroups(arr, n);
+  // Explicitly handling the end of
+  // last interval
+  if (arr[n - 1] != arr[0])
+    console.log(n - 1 + "<br>");
+}
+  
+var arr = [0, 1, 1, 0, 0, 0, 1, 1];
+var n = arr.length;
+printGroups(arr, n);
